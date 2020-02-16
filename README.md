c932bd244eb09a704277d4554fba68d1b2421fbe
nlebedevinc <nlebedevme@gmail.com>
2020-02-16 22:48:18 +0300

    nvmrc and readme files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Is URL
+
+TBD
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function isURL(string) {
+/**
+ * Checks if the given string is URL
+ * @param {string} string
+ * @returns {boolean}
+ */
+function isURL(pattern) {
     try {
         const decoded = decodeURIComponent(pattern);
         const url = new URL(decoded);
